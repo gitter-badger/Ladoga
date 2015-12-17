c430a37a16c7b605063d3cbd55aad0d9857aaafb
Alexander Perechnev <aperechnev@storios.ru>
2015-12-17 07:22:58 +0300

    LDTCPServer docs.

--- Ladoga/Ladoga/LDTCPServer.h
+++ Ladoga/Ladoga/LDTCPServer.h
@@ -15,21 +15,33 @@
 @end
 
 
-
+/**
+ @brief An class which implements the base TCP server.
+ 
+ @discussion Actually you don't need to use this class since you want to run
+    an HTTP server. Use LDHTTPServer class, which inherits LDTCPServer and 
+    provides required functionality.
+ */
 @interface LDTCPServer : NSObject
 
 /**
- IP address of interface where server is running.
+ @brief IP address of interface where server is running.
+ 
+ @discussion The default value is 0.0.0.0, which means that server is available
+                an all existing interfaces.
  */
 @property (nonatomic, strong, readonly) NSString *address;
 
 /**
- Port number that server is binded to.
+ @brief Port number that server is binded to.
+ 
+ @discussion The default value is 0, which means that server will choose one of
+                available ports and bind to it.
  */
 @property (nonatomic, assign, readonly) NSInteger port;
 
 /**
- An callback that will be called by server when new connection accepted.
+ @brief An callback that will be called by server when new connection accepted.
  */
 @property (nonatomic, weak, readwrite) id <LDTCPServerDelegate> tcpServerDelegate;
 
@@ -40,24 +52,24 @@
 - (instancetype)init NS_UNAVAILABLE;
 
 /**
- Initializes server with required parameters.
+ @brief Initializes server with required parameters.
  
- address: 
- IP-address of interface that server should be binded to. Default is 0.0.0.0
+ @param IP-address of interface that server should be binded to. Default is 0.0.0.0.
+ @param Port number that should be used to listen new connections.
  
- port: 
- Port number that should be used to listen new connections.
+ @return Returns an initialized, ready-to-run instance.
  */
 - (instancetype)initWithAddress:(NSString *)address
                         andPort:(NSInteger)port;
 
 /**
- Binds server to TCP port and start listening for new connections
+ @brief Binds server to TCP port and start listening for new connections.
+ @return Return value indicates if server runned successfully.
  */
 - (BOOL)startWithRunLoop:(CFRunLoopRef)runLoop;
 
 /**
- Stops server
+ @brief Stops server.
  */
 - (void)stop;
 
